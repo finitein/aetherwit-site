f865b2e3e02255de60472e81a22b7cc16b0b4ad0
finitein <274449224+finitein@users.noreply.github.com>
2026-06-23 23:08:40 +0800

    chore: update Tina generated files

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ cacheDir: '/home/henry/all-projects/aetherwit-site/tina/__generated__/.cache/1782227246938', url: 'https://content.tinajs.io/2.4/content/ace6c9f9-627e-4f91-83e2-e19bad791f04/github/main', token: '87f9812b3281ddbb250df6289baf56261d321daa', queries,  });
 export default client;
   
